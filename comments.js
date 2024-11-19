23f515d9ed30f308a3a3d973ff4fed1e870efc69
ManjuRukman <manjupriya.r@ilink-systems.com>
2024-11-19 14:31:57 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,49 +1,25 @@
 // create web server
-var express = require('express');
-var app = express();
-var fs = require('fs');
-var bodyParser = require('body-parser');
-var jsonParser = bodyParser.json();
-var urlencodedParser = bodyParser.urlencoded({extended: false});
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+const port = 3000;
 
-// create server
-var server = app.listen(8081, function(){
-    var host = server.address().address;
-    var port = server.address().port;
-    console.log('Server is listening at http://%s:%s', host, port);
+// get comments
+app.get('/comments', (req, res) => {
+    res.json([
+        {
+            id: 1,
+            name: 'John Doe',
+            comment: 'This is a comment'
+        },
+        {
+            id: 2,
+            name: 'Jane Doe',
+            comment: 'This is another comment'
+        }
+    ]);
 });
 
-// get request
-app.get('/comments', function(req, res){
-    fs.readFile(__dirname + '/' + 'comments.json', 'utf8', function(err, data){
-        if(err){
-            console.log(err);
-        }else{
-            console.log(data);
-            res.end(data);
-        }
-    });
-});
-
-// post request
-app.post('/comments', urlencodedParser, function(req, res){
-    var comment = {
-        name: req.body.name,
-        comment: req.body.comment
-    };
-    fs.readFile(__dirname + '/' + 'comments.json', 'utf8', function(err, data){
-        if(err){
-            console.log(err);
-        }else{
-            data = JSON.parse(data);
-            data.push(comment);
-            fs.writeFile(__dirname + '/' + 'comments.json', JSON.stringify(data), function(err){
-                if(err){
-                    console.log(err);
-                }else{
-                    res.end(JSON.stringify(data));
-                }
-            });
-        }
-    });
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
 });
